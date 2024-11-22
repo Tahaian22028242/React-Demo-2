52a4c5abf769e7bb84aa5f83f73067fb7aa1fb06
TaHaiAn <22028242@vnu.edu.vn>
2024-11-22 16:56:50 +0700

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,18 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import Login from './pages/Login';
 
 function App() {
   return (
-    <BrowserRouter basename='/React-Demo-2/'>
-        <Routes>
-          <Route path="/" element={<Navigate to="/login" />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/login" element={<Login />} />
-        </Routes>
-    </BrowserRouter>
+    <Router basename='/React-Demo-2'>
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
   );
 }
 
